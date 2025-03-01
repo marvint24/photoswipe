9bf8989fc6f1d6d225f1bd6d7ed5b2bc1aee46c4
marvint24 <marvin.terme@gmail.com>
2025-03-01 22:53:43 +0100

    getting started

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
+
+<style>
+  div{
+    text-emphasis-color: red;
+  }
+</style>
+
+<h1>Welcome to SvelteKit How are you</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<div>
+    <p>Test</p>
+    <button>Test1 </button> <button> test 2</button>
+</div>
