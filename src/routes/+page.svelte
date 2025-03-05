7842e399bc95b9352bc0f119af1350b815cf1c82
Your Name <lenny0908@googlemail.com>
2025-03-05 21:06:09 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,16 @@
   div{
     text-emphasis-color: red;
   }
+
 </style>
+
+<script>
+  alert("hji")
+</script>
 
 <h1>Welcome to SvelteKit How are you</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Lenny war hier</p>
 
 
 <div>
